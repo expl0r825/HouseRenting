f85e351329d8d11443937da5805474e3e808d06d
expl0r825 <martom825@gmail.com>
2025-03-22 17:15:39 +0200

    Added api and provider

---
+++ client/src/api/authApi.js
@@ -0,0 +1,56 @@
+import { useContext, useEffect } from "react";
+
+import request from "../utils/request"
+import { UserContext } from '../contexts/UserContext'
+
+const baseUrl = 'http://localhost:3030/users';
+
+export const useLogin = () => {
+    const login = async (email, password) => {
+        request.post(
+            `${baseUrl}/login`,
+            { email, password },
+        );
+
+        return {
+            login,
+        }
+    };
+};
+
+export const useRegister = () => {
+    const register = (email, password) => {
+        request.post(
+            `${baseUrl}/register`,
+            { email, password },
+        );
+    }
+
+    return {
+        register,
+    }
+};
+
+export const useLogout = () => {
+    const { accessToken, userLogoutHandler } = useContext(UserContext);
+
+    useEffect(() => {
+        if (!accessToken) {
+            return;
+        }
+
+        const options = {
+            header: {
+                'X-Authorization': accessToken,
+            }
+        };
+
+        request.get(`${baseUrl}/logout`, null, options)
+            .then(userLogoutHandler);
+
+    }, [accessToken, userLogoutHandler]);
+
+    return {
+        isLoggedOut: !!accessToken,
+    }
+};
